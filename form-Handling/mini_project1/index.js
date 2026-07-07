091bec8301344ecceeeb61e34d8583d411181c3a
Ayush Chand <ayushchand862@gmail.com>
2026-07-07 23:18:13 +0530

    Done with storing data of notes in file folder and showing them on ui also completed the routing part

--- form-Handling/mini_project1/index.js
+++ form-Handling/mini_project1/index.js
@@ -1,27 +1,42 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const fs = require("fs")
-
+const fs = require('fs');
 
 app.set("view engine", "ejs");
+
 app.use(express.json());
-app.use(express.urlencoded({ extended : true }));
-app.use(express.static(path.join(__dirname , "public")));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get('/',function(req,res){
-    fs.readdir(`./file` , function(err,files){
-        res.render('index',{files: files});
-    })
-    
-})
+app.get('/', (req, res) => {
+    fs.readdir("./file", (err, files) => {
+        if (err) return res.status(500).send(err.message);
 
-app.post('/create',function(req,res){
-    console.log(req.body)
-    fs.writeFile(`./file/${req.body.title.split(' ').join('')}` , req.body.details , (err)=>{
-        res.redirect('/')
-    })
-    
-})
+        res.render("index", { files });
+    });
+});
 
-app.listen(3000);
+app.get('/file/:filename', (req, res) => {
+    fs.readFile(`./file/${req.params.filename}`, "utf-8", (err, filedata) => {
+        if (err) return res.status(404).send("File not found");
+
+        res.render('show', {filename : req.params.filename , filedata : filedata});
+    });
+});
+
+app.post('/create', (req, res) => {
+    fs.writeFile(
+        `./file/${req.body.title.split(" ").join("")}`,
+        req.body.details,
+        (err) => {
+            if (err) return res.status(500).send(err.message);
+
+            res.redirect("/");
+        }
+    );
+});
+
+app.listen(3000, () => {
+    console.log("Server running on http://localhost:3000");
+});
