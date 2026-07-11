70c42b2e7bb32df11b8b3e6cfb56b4cf5cc4cb32
Ayush Chand <ayushchand862@gmail.com>
2026-07-11 21:35:20 +0530

    Learned how to CRUD oprn with ejs and server side rendering and also used them to create a application

---
+++ CRUD_opr_with_ejs/app.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+const userModel = require('./models/user')
+
+app.set('view engine' , 'ejs')
+app.use(express.json())
+app.use(express.urlencoded({extended : true}));
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/' , (req,res)=>{
+    res.render('index')
+})
+
+app.get('/delete/:id' , async (req,res)=>{
+     let users = await userModel.findOneAndDelete({_id: req.params.id});
+     res.redirect("/read")
+})
+
+app.get('/read' , async (req,res)=>{
+    let users = await userModel.find()
+    res.render('read', {users})
+})
+
+app.post('/create' , async (req,res)=>{
+    let {name , email , image} = req.body;
+    let createduser  = await userModel.create({
+        name:name,
+        email:email,//if you have same vame on both side you can write it only one time
+        image
+    })
+
+    res.redirect('/read');
+})
+app.listen(3000)
